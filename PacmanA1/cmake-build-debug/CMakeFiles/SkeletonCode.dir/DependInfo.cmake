8db26076a87c86a62f81be4662c57f9567f9dfac
Christopher McArtur <prince.chrismc@gmail.com>
2017-09-21 11:21:36 -0400

    Renamed assignment 1 files and directories

---
+++ PacmanA1/cmake-build-debug/CMakeFiles/SkeletonCode.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/charalambos/Documents/CODE/COMP371/2017/FALL/SkeletonCode/main.cpp" "/home/charalambos/Documents/CODE/COMP371/2017/FALL/SkeletonCode/cmake-build-debug/CMakeFiles/SkeletonCode.dir/main.cpp.o"
+  "/home/charalambos/Documents/CODE/COMP371/2017/FALL/SkeletonCode/objloader.cpp" "/home/charalambos/Documents/CODE/COMP371/2017/FALL/SkeletonCode/cmake-build-debug/CMakeFiles/SkeletonCode.dir/objloader.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CMAKE_BUILD_TYPE=Release"
+  "GLM_FORCE_RADIANS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ PacmanA1/cmake-build-debug/CMakeFiles/SkeletonCode.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SkeletonCode.dir/main.cpp.o"
+  "CMakeFiles/SkeletonCode.dir/objloader.cpp.o"
+  "../bin/SkeletonCode.pdb"
+  "../bin/SkeletonCode"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SkeletonCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
