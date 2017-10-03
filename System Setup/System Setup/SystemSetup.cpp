94030105deef5124bad4adc76962a2f5cba3bcbb
Christopher McArthur <prince.chrismc@gmail.com>
2017-10-03 19:02:06 -0400

    now create a process which calls a batch file
    runtime error since the file does not exist

--- System Setup/System Setup/SystemSetup.cpp
+++ System Setup/System Setup/SystemSetup.cpp
@@ -1,11 +1,61 @@
 // SystemSetup.cpp : Defines the entry point for the console application.
 //
 
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
 #include <iostream>
+#include <sstream>
+#include "SystemSetup.h"
 
 int main()
 {
    std::cout << "Hello World!" << std::endl;
+
+   STARTUPINFO si;
+   PROCESS_INFORMATION pi;
+
+   ZeroMemory(&si, sizeof(si));
+   si.cb = sizeof(si);
+   ZeroMemory(&pi, sizeof(pi));
+   if (!CreateProcess(NULL,
+      L"cmd /C  hello.bat",
+      NULL,
+      NULL,
+      FALSE,
+      0,
+      NULL,
+      NULL,
+      &si,
+      &pi)
+      )
+   {
+      printf("CreateProcess failed (%d)\n", GetLastError());
+      return FALSE;
+   }
+   WaitForSingleObject(pi.hProcess, INFINITE);
+   CloseHandle(pi.hProcess);
+   CloseHandle(pi.hThread);
+
    return 0;
 }
 
+const uint16_t GetUserInput(const uint16_t & lower, const uint16_t & upper)
+{
+   // this is a gneral utility function to get a valid input between two values (inclusively)
+   // adding this removed aprox. 200 lines of code lol
+   uint16_t selection = 0;
+   do
+   {
+      std::cout << "Selcetion: ";
+      std::string input;
+      std::getline(std::cin, input);
+      std::stringstream ss(input);
+      ss >> selection;
+
+      if (selection < lower || selection > upper)
+         std::cout << "Invalid option. Please Try again..." << std::endl;
+
+   } while (selection < lower || selection > upper);
+
+   return selection;
+}
---
+++ System Setup/System Setup/SystemSetup.h
@@ -0,0 +1,3 @@
+#pragma once
+
+const uint16_t GetUserInput(const uint16_t & lower, const uint16_t & upper);
