b64a3b884785501d1503961fdb4ba1e9d7c0bd70
Christopher McArtur <prince.chrismc@gmail.com>
2017-10-04 15:17:48 -0400

    Beginning of template vcxproj uses env vars

---
+++ System Setup/Template/Template.cpp
@@ -0,0 +1,13 @@
+// Template.cpp : Defines the entry point for the console application.
+//
+
+
+#include "GL/glew.h"       // include GL Extension Wrangler
+#include "GLFW/glfw3.h"    // include GLFW helper library
+#include "glm/glm.hpp"     // include GLM library
+
+int main()
+{
+    return 0;
+}
+
