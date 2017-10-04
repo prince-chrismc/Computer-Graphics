c773381d1cc2c75625bf3c5a7891003fefea0076
Christopher McArtur <prince.chrismc@gmail.com>
2017-10-04 16:49:11 -0400

    Template now compiles and spawns a GLFW windows

--- System Setup/Template/Template.cpp
+++ System Setup/Template/Template.cpp
@@ -1,13 +1,59 @@
+/*
+MIT License
+
+Copyright (c) 2017 Chris McArthur, prince.chrismc(at)gmail(dot)com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 // Template.cpp : Defines the entry point for the console application.
 //
 
+#include <iostream>
 
 #include "GL/glew.h"       // include GL Extension Wrangler
-#include "GLFW/glfw3.h"    // include GLFW helper library
-#include "glm/glm.hpp"     // include GLM library
+
+#include "GlfwWindow.h"
 
 int main()
 {
-    return 0;
+   std::cout << "Welcome to Template!" << std::endl;
+
+   // Create a GLFWwindow
+   GlfwWindow window(GlfwWindow::DEFAULT_WIDTH, GlfwWindow::DEFAULT_HEIGHT);
+   if (!window()) // Make sure it exists
+   {
+      return -1;
+   }
+
+   // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+   glewExperimental = GL_TRUE;
+   // Initialize GLEW to setup the OpenGL Function pointers
+   if (glewInit() != GLEW_OK)
+   {
+      std::cout << "Failed to initialize GLEW" << std::endl;
+      return -1;
+   }
+
+
+
+
+   return 0;
 }
 
