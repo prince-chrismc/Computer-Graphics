829ee6da45bec081ecb22207adfe6e2cd67a69e7
Chris McArthur <prince.chrismc@gmail.com>
2017-10-29 15:05:52 -0400

    updated shader to modern c++ singleton

--- System-Setup/Template/src/Template.cpp
+++ System-Setup/Template/src/Template.cpp
@@ -47,9 +47,9 @@ int main()
 
    // Set the required callback functions
    /*
-   * window.SetKeyCallback(key_callback);
-   * window.SetMouseButtonCallback(mouse_callback);
-   * window.SetCursorPosCallback(cursor_callback);
+   * window->SetKeyCallback(key_callback);
+   * window->SetMouseButtonCallback(mouse_callback);
+   * window->SetCursorPosCallback(cursor_callback);
    */
 
    if (glewInit() != GLEW_OK)                   // Initialize GLEW to setup the OpenGL Function pointers
@@ -70,7 +70,7 @@ int main()
       return -1;
    }
 
-   ShaderLinker* shaderProgram = &ShaderLinker::GetInstance();
+   auto shaderProgram = ShaderLinker::GetInstance();
    if (!shaderProgram->Link(&vertexShader, &fragmentShader))
    {
       std::cout << "Press 'enter' to exit." << std::endl;
