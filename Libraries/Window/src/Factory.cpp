155f8f916e95f95087089d537bce268061be8729
Chris McArthur <prince.chrismc@gmail.com>
2017-12-27 16:53:08 -0500

    renamed file glfwwindow.* to factory.*

--- Libraries/Window/src/Factory.cpp
+++ Libraries/Window/src/Factory.cpp
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#include "GlfwWindow.h"
+#include "Factory.h"
 #include "glm/gtc/matrix_transform.hpp"
 #include <iostream>
 
